fe75670663cd694e3e8061a118f969b84334e118
André R.B. (mac) <bronca.andre@gmail.com>
2022-08-13 10:33:35 -0300

    Criando o discovery service

---
+++ discovery_service_eureka/src/main/java/com/arb/discovery_service_eureka/DiscoveryServiceEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.arb.discovery_service_eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryServiceEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServiceEurekaApplication.class, args);
+	}
+
+}
---
+++ discovery_service_eureka/src/test/java/com/arb/discovery_service_eureka/DiscoveryServiceEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arb.discovery_service_eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
