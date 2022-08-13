5bb4f631c2c834743da6491e6b763b9f6da9af6c
André R.B. (mac) <bronca.andre@gmail.com>
2022-08-13 15:21:57 -0300

    configuração do gateway (zuul) para comunicação com o discovery (eureka). Obs. Esse projeto está funcionado, subindo as aplicações, ocorreram diversos erros, então até aqui funciona.

---
+++ gateway_service_zuul/src/main/java/com/arb/gateway_service_zuul/GatewayServiceZuulApplication.java
@@ -0,0 +1,17 @@
+package com.arb.gateway_service_zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class GatewayServiceZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServiceZuulApplication.class, args);
+	}
+
+}
---
+++ gateway_service_zuul/src/test/java/com/arb/gateway_service_zuul/GatewayServiceZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arb.gateway_service_zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
