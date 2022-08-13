951426a294b60eaec3ce38be2e22f94306fa53b4
André R.B. (mac) <bronca.andre@gmail.com>
2022-08-13 11:06:16 -0300

    configuração para dar start ao discovery service eureka

--- discovery_service_eureka/src/main/java/com/arb/discovery_service_eureka/DiscoveryServiceEurekaApplication.java
+++ discovery_service_eureka/src/main/java/com/arb/discovery_service_eureka/DiscoveryServiceEurekaApplication.java
@@ -2,8 +2,10 @@ package com.arb.discovery_service_eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class DiscoveryServiceEurekaApplication {
 
 	public static void main(String[] args) {
